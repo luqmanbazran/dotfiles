991cd26f8c2fa7396a30c9a9ed1e455c265b3fde
lbazran <luqman.bazran@frgrisk.com>
2024-02-13 13:50:18 +0800

    Added some comments for better description

--- check_installation.sh
+++ check_installation.sh
@@ -28,16 +28,18 @@ else
     echo "zsh is not installed"
 fi
 
+#Compare hash of installed .zshrc and .vimrc file to see if the files are the same
 zshrc_md5=($(md5sum ~/.zshrc | awk '{print $1}'))
 vimrc_md5=($(md5sum ~/.vimrc | awk '{print $1}'))
 
-
+#Compare .zshrc hash
 if [ $zshrc_md5 == "4d3bf12f71e2781cd2182b1fb819ba2b" ]; then
     echo ".zshrc file is configured properly"
 else
     echo ".zshrc file is not configured properly"
 fi
 
+#Compare .vimrc file
 if [ $vimrc_md5 == "7983a3c3e209b215af87d2e9b9d539ba" ]; then
     echo ".vimrc file is configured properly"
 else
