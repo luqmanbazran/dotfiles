759beccdf5218d07b75b38da1d31ea55c0df2b5c
lbazran <luqman.bazran@frgrisk.com>
2024-02-14 09:09:22 +0800

    Fixed directory path from which file is copied

--- dev_setup.sh
+++ dev_setup.sh
@@ -6,14 +6,15 @@ dnf update
 dnf -y install git vim tmux zsh
 
 #Clone a github repo containing the .zshrc and .vimrc to be configured
-git clone https://github.com/luqmanbazran/dotfiles.git ~/
+cd ~/
+git clone https://github.com/luqmanbazran/dotfiles.git
 
 #Configure .zshrc 
-cp learnBash/.zshrc ~/
+cp dotfiles/.zshrc ~/
 
 #Configure .vimrc
 mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
-cp learnBash/.vimrc ~/
+cp dotfiles/.vimrc ~/
 
 #Change the default shell to zsh
 chsh -s $(which zsh)
